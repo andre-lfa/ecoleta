5e1e5259629e93431160f263a54789e8af5ac36e
Andre LFA <andrelfaareas@gmail.com>
2020-06-02 14:12:29 -0300

    Entendendo rotas e parâmetros de forma simples

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,41 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Hello World'); 
+app.use(express.json());
 
-    response.json([
-        "Hello World"  
-    ]);
-}) 
+const users = [
+    'Daniel',
+    'André',
+    'Gabriel',
+    'Rafael'
+]
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const search = users[id]
+
+    return response.json(search);
+});
+
+app.get('/users', (request, response) => {
+    const query = String(request.query.search);
+
+    const filteredUsers = query ? users.filter(user => user.includes(query)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email
+    }
+
+    return response.json(user);
+}); 
 
 app.listen(3333); 
