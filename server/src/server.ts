cd70e71f47a77e54917a8e453a887b4bf7170cc4
Andre LFA <andrelfaareas@gmail.com>
2020-06-02 14:43:13 -0300

    Desacopla as rotas da aplicação para o arquivo routes.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,10 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const users = [
-    'Daniel',
-    'André',
-    'Gabriel',
-    'Rafael'
-]
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const search = users[id]
-
-    return response.json(search);
-});
-
-app.get('/users', (request, response) => {
-    const query = String(request.query.search);
-
-    const filteredUsers = query ? users.filter(user => user.includes(query)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-
-    return response.json(user);
-}); 
+app.use(routes);
 
 app.listen(3333); 
