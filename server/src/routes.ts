f03ef3f4129e604f3f1845e2c977e1608804b773
Andre LFA <andrelfaareas@gmail.com>
2020-06-03 14:48:02 -0300

    Cria a rota para criar pontos de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from '../src/database/connection';
 
 const routes = express.Router();
@@ -8,12 +8,51 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
     })
 
     return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-inicial',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: insertedIds[0],
+        };
+    });
+
+    await trx('points_items').insert(pointItems); 
+
+    return response.json({ success: true });
+
 });
 
 export default routes;
