4fd20ace5ad8dbca687166fa80fb639a082188aa
Andre LFA <andrelfaareas@gmail.com>
2020-06-04 15:33:01 -0300

    Entendendo os principais conceitos do React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello World
-        </h1>
-      </header>
+    <div>  
+      <Header title="Ecoleta"/>
+      <h1>{counter}</h1>
+      <button type="button" onClick={increment}>Incrementar</button>
     </div>
   );
 }
