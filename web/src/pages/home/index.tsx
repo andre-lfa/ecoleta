72a200d0a61e8493de3c90824c3605c87b7343a4
Andre LFA <andrelfaareas@gmail.com>
2020-06-04 16:56:39 -0300

    Cria e configura as rotas da aplicação

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -14,12 +15,12 @@ const Home = () => {
                 <main>
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente!</p>
-                    <a href="/cadastro">
+                    <Link to="/create">
                         <span>
                             <FiLogIn/>
                         </span>
                         <strong>Cadastre um ponto de coleta</strong>
-                    </a>
+                    </Link>
                 </main>
             </div>
         </div>
