3b2d068fd13c958f8ac0a83b9204253951d0a064
Andre LFA <andrelfaareas@gmail.com>
2020-06-09 04:36:18 -0300

    Cria o componente dropzone para lidar com upload de imagens

---
+++ web/src/components/Dropzone/index.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { FiUpload } from 'react-icons/fi';
+import './styles.css';
+
+const Dropzone = () => {
+
+    const [selectedFileURL, setSelectedFile] = useState('');
+
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0];
+
+        const fileUrl = URL.createObjectURL(file);
+
+        setSelectedFile(fileUrl);
+
+    }, [])
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+        accept: 'image/*'
+    })
+
+    return (
+        <div className='dropzone' {...getRootProps()}>
+            <input {...getInputProps()} accept='image/*' />
+            {
+                selectedFileURL ? <img src={selectedFileURL} alt="Point" /> : (
+                    <p>
+                        <FiUpload />
+                        Imagem do estabelecimento
+                    </p>
+                )
+            }
+        </div>
+    )
+}
+
+export default Dropzone;
